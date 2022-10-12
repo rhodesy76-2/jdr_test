903eeb24993f7b3db24a56a8ecedafc2d3783f8e
rhodesy76-2 <113443029+rhodesy76-2@users.noreply.github.com>
2022-10-12 13:41:01 +0200

    Commit Hangman milestone_1

--- milestone_1_test.py
+++ milestone_1_test.py
@@ -1,4 +1,4 @@
-#%%
+
 # Write import random on the line. Note: To import a module, you have to use the import keyword at the top of the file.
 import random
 
@@ -8,7 +8,7 @@ import random
 # Step 2. Assign this list to a variable called word_list
 # Step 3. Print out the newly created list to the standard output (screen).
 
-#%%
+
 word_list = ["banana", "apple", "kiwi", "orange", "pear"]
 print(word_list)
 
@@ -22,12 +22,11 @@ word = random.choice(word_list)
 print(word)
 
 
-# %%
 # Step 1. Using the input function, ask the user to enter a single letter.
 # Step 2. Assign the input to a variable called guess.
-guess=input("Please input a single letter")
+guess = input("Please input a single letter")
 print(guess)
-# %%
+
 # Step 1. Create an if statement that checks if the length (lens) of the input is equal to 1 and the input is an alphabet (isalpha).
 # Step 2: In the body of the if statement, print a message that says "Good guess!".
 # Step 3: Create an else block that prints "Oops! That is not a valid input." if the preceeding conditions are not met.
@@ -36,4 +35,6 @@ if len(guess) == 1 and guess.isalpha():
     print("Good guess!")
 else :
     print("Oops! That is not a valid input.")
-# %%
+
+
+
